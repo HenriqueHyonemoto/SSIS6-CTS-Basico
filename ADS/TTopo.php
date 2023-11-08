a63f37ccac35ffe20eced5333f36cbe1df279908
Henrique Hyonemoto <91375748+HenriqueHyonemoto@users.noreply.github.com>
2023-11-08 14:42:35 -0300

    Delete ADS directory

--- ADS/TTopo.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Aproveitem a Prova " . date('Y');
-header("Expires: Mon, 26 maio 1997 09:00:00 GMT");
-header("Cache-Control: no-cache");
-header("Pragma: no-cache");
-?>
--- ADS/bb.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["nomevai"];
-$_SESSION["ravai"];
-session_destroy();
-header("Location: http://ndlvtp.org/ava/ADS/", TRUE);
-?>
